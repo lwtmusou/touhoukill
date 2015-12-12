d439ae1f509750ced7e4933dc49f577ae6362ca4
lwtmusou <lwtmusou@126.com>
2015-12-12 09:58:55 +0900

    杂项

--- src/ui/startscene.cpp
+++ src/ui/startscene.cpp
@@ -18,7 +18,7 @@ StartScene::StartScene()
     QFont website_font(Config.SmallFont);
     website_font.setStyle(QFont::StyleItalic);
     //QGraphicsSimpleTextItem *website_text = addSimpleText("http://qsanguosha.org", website_font);
-    QGraphicsSimpleTextItem *website_text = addSimpleText("http://mogara.org", website_font);
+    QGraphicsSimpleTextItem *website_text = addSimpleText("东方杀QQ群: 384318315", website_font);
     website_text->setBrush(Qt::white);
     website_text->setPos(Config.Rect.width() / 2 - website_text->boundingRect().width(),
         Config.Rect.height() / 2 - website_text->boundingRect().height());
