f7cf30dd93cd0d7da98445e4713b750a35301430
Fs <Fsu0413@gmail.com>
2016-01-03 01:18:11 +0800

    修崩

--- lua/general_select.lua
+++ lua/general_select.lua
@@ -1,39 +1,39 @@
 
 initialize = function()
-	sgs.Sanguosha:currentRoom():getPlayers():first():speak("generalselector initialize")
-end,
+	--sgs.Sanguosha:currentRoom():getPlayers():first():speak("generalselector initialize")
+end
 
 selectFirst = function(player, candidates) -- string
 	player:speak("selectfirst")
-	return nil
-end,
+	return candidates[math.random(#candidates)]
+end
 
 selectSecond = function(player, candidates) -- string
 	player:speak("selectSecond")
-	return nil
-end,
+	return candidates[math.random(#candidates)]
+end
 
 select1v1 = function(candidates) -- string
 	player:speak("select1v1")
-	return nil
-end,
+	return candidates[math.random(#candidates)]
+end
 
 select3v3 = function(player, candidates) -- string
 	player:speak("select3v3")
-	return nil
-end,
+	return candidates[math.random(#candidates)]
+end
 
 arrange3v3 = function(player) -- stringlist
 	player:speak("arrange3v3")
 	return {}
-end,
+end
 
 arrange1v1 = function(player) -- stringlist
 	player:speak("arrange1v1")
 	return {}
-end,
+end
 
 get1v1ArrangeValue = function(name) -- int
 	player:speak("get1v1ArrangeValue")
 	return 0
-end,
+end
