4d4eb1521f0e04701620accc5aeac610485b3b03
Fs <Fsu0413@gmail.com>
2016-01-08 01:38:20 +0800

    由于通信协议更改，新版不能和旧版联机，所以先修改版本号防止联机

--- src/core/engine.cpp
+++ src/core/engine.cpp
@@ -644,7 +644,7 @@ SkillCard *Engine::cloneSkillCard(const QString &name) const
 
 QString Engine::getVersionNumber() const
 {
-    return "20151212";
+    return "20160108";
 }
 
 QString Engine::getVersion() const
@@ -654,7 +654,7 @@ QString Engine::getVersion() const
 
 QString Engine::getVersionName() const
 {
-    return "V0.70";
+    return "V0.80";
 }
 
 QString Engine::getMODName() const
