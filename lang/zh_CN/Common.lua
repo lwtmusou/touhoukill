42d43850171004b1ac6823c65df33033b00f4815
Fs <Fsu0413@gmail.com>
2016-01-28 08:05:39 +0800

    翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译翻译

--- lang/zh_CN/Common.lua
+++ lang/zh_CN/Common.lua
@@ -185,6 +185,7 @@ CommonTranslationTable = {
 	["$CancelTarget"] = "%from 使用【%arg】的目标 %to 被取消",
 	["$CancelTargetNoUser"] = "【%arg】的目标 %to 被取消",
 	["#UseLuckCard"] = "%from 使用了 <font color=\"yellow\"><b>手气卡</b></font>",
+	["#ForbidExtraTurn"] = "由于场上存在其他额外回合效果， %from 的额外回合效果被防止" ,
     
 	["TriggerOrder"] = "选择触发顺序",
 	["GameRule:TriggerOrder"] = "请选择先发动的技能",
