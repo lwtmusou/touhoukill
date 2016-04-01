11a2e4f5b1ddfe3875e81572dcb201dd067ec7ad
lwtmusou <lwtmusou@126.com>
2016-04-01 18:15:26 +0900

    旧作包编号变动

--- src/core/engine.cpp
+++ src/core/engine.cpp
@@ -612,7 +612,7 @@ SkillCard *Engine::cloneSkillCard(const QString &name) const
 
 QString Engine::getVersionNumber() const
 {
-    return "20160320";
+    return "20160401";
 }
 
 QString Engine::getVersion() const
--- src/package/th01-05.cpp
+++ src/package/th01-05.cpp
@@ -526,16 +526,9 @@ public:
 TH0105Package::TH0105Package()
     : Package("th0105")
 {
-    General *shinki = new General(this, "shinki$", "pc98", 4, false);
-    Q_UNUSED(shinki);
-    General *alice = new General(this, "alice_old", "pc98", 4, false);
-    Q_UNUSED(alice);
-    General *yuka = new General(this, "yuka_old$", "pc98", 4, false);
-    Q_UNUSED(yuka)
-    General *gengetsumugetsu = new General(this, "gengetsumugetsu", "pc98", 4, false);
-    Q_UNUSED(gengetsumugetsu);
-    General *elly = new General(this, "elly", "pc98", 4, false);
-    Q_UNUSED(elly);
+    General *mima = new General(this, "mima$", "pc98", 4, false);
+    Q_UNUSED(mima);
+
     General *yumemi = new General(this, "yumemi$", "pc98", 4, false);
     yumemi->addSkill(new Ciyuan);
     yumemi->addSkill(new Shigui);
@@ -551,9 +544,17 @@ TH0105Package::TH0105Package()
     kana->addSkill(new Qianyi);
     kana->addSkill(new Mengxiao);
 
+    General *yuka = new General(this, "yuka_old$", "pc98", 4, false);
+    Q_UNUSED(yuka)
+    General *gengetsumugetsu = new General(this, "gengetsumugetsu", "pc98", 4, false);
+    Q_UNUSED(gengetsumugetsu);
+    General *elly = new General(this, "elly", "pc98", 4, false);
+    Q_UNUSED(elly);
 
-    General *mima = new General(this, "mima$", "pc98", 4, false);
-    Q_UNUSED(mima);
+    General *shinki = new General(this, "shinki$", "pc98", 4, false);
+    Q_UNUSED(shinki);
+    General *alice = new General(this, "alice_old", "pc98", 4, false);
+    Q_UNUSED(alice);
 }
 
 ADD_PACKAGE(TH0105)
