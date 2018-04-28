45792012725f3c04f75ef68fac365ea82b8357d6
lwtmusou <lwtmusou@126.com>
2018-04-28 11:50:59 +0900

    捣药ai

--- lua/ai/th99-ai.lua
+++ lua/ai/th99-ai.lua
@@ -1291,7 +1291,8 @@ sgs.ai_skill_playerchosen.daoyao = function(self, targets)
 	local target_table = sgs.QList2Table(targets)
 	self:sort(target_table, "handcard")
 	for _,p in ipairs(target_table) do
-		if self:isFriend(p) and p:isChained() then
+		if self:isFriend(p) and self.player:objectName() ~= p:objectName() 
+		    and (p:isChained() == self.player:isChained()) then
 			return p
 		elseif p:isCurrent() and  self:isEnemy(p) and p:canDiscard(p, "hes") then
 			return p
