dba31e26e7860c6d03a16e6769b17515c03a00a1
lwtmusou <lwtmusou@126.com>
2019-08-24 14:30:49 +0900

    矢田寺成美 试做
    缺log

--- src/package/th16.cpp
+++ src/package/th16.cpp
@@ -1,6 +1,7 @@
 #include "th16.h"
 #include "general.h"
 #include "skill.h"
+#include "engine.h"
 
 MenfeiCard::MenfeiCard()
 {
@@ -152,6 +153,163 @@ public:
 };
 
 
+class Puti : public TriggerSkill
+{
+public:
+    Puti()
+        : TriggerSkill("puti")
+    {
+        events << EventPhaseStart << EventPhaseChanging;
+    }
+
+    void record(TriggerEvent triggerEvent, Room *room, QVariant &data) const
+    {
+        if (triggerEvent == EventPhaseChanging) {
+            PhaseChangeStruct change = data.value<PhaseChangeStruct>();
+            if (change.from == Player::Play) {
+                if (change.player->isCardLimited("use", "puti"))
+                    room->removePlayerCardLimitation(change.player, "use", "Slash$1", "puti");
+            }
+        }
+    }
+
+    QList<SkillInvokeDetail> triggerable(TriggerEvent e, const Room *, const QVariant &data) const
+    {
+        if (e == EventPhaseStart) {
+            ServerPlayer *player = data.value<ServerPlayer *>();
+            if (!player->hasSkill(this) || player->isDead() || player->getPhase() != Player::Play)
+                return QList<SkillInvokeDetail>();
+            return QList<SkillInvokeDetail>() << SkillInvokeDetail(this, player, player);
+        
+        }
+
+        return QList<SkillInvokeDetail>();
+    }
+
+
+    bool effect(TriggerEvent, Room *room, QSharedPointer<SkillInvokeDetail>invoke, QVariant &data) const
+    {
+        ServerPlayer *player = invoke->invoker;
+        int acquired = 0;
+        QList<int> throwIds;
+        while (acquired < 1) {
+            int id = room->drawCard();
+            CardsMoveStruct move(id, NULL, Player::PlaceTable, CardMoveReason(CardMoveReason::S_REASON_TURNOVER, player->objectName()));
+            move.reason.m_skillName = "puti";
+            room->moveCardsAtomic(move, true);
+            room->getThread()->delay();
+            Card *card = Sanguosha->getCard(id);
+            if (card->getTypeId() == Card::TypeTrick) {
+                acquired = acquired + 1;
+                CardsMoveStruct move2(id, player, Player::PlaceHand, CardMoveReason(CardMoveReason::S_REASON_GOTBACK, player->objectName()));
+                room->moveCardsAtomic(move2, true);
+
+                room->setPlayerCardLimitation(player, "use", "Slash", "puti", true);
+                if (!throwIds.isEmpty()) {
+                    CardMoveReason reason(CardMoveReason::S_REASON_NATURAL_ENTER, player->objectName(), "sishu", QString());
+                    DummyCard dummy(throwIds);
+                    room->throwCard(&dummy, reason, NULL);
+                    throwIds.clear();
+                }
+            }
+            else
+                throwIds << id;
+        }
+
+        return false;
+    }
+};
+
+
+class Zangfa : public TriggerSkill
+{
+public:
+    Zangfa()
+        : TriggerSkill("zangfa")
+    {
+        events << TargetSpecifying;
+    }
+
+    QList<SkillInvokeDetail> triggerable(TriggerEvent , const Room *room, const QVariant &data) const
+    {
+        CardUseStruct use = data.value<CardUseStruct>();
+        if (!use.card->isNDTrick())
+            return QList<SkillInvokeDetail>();
+
+        bool invoke = false;
+        use.card->setFlags("IgnoreFailed");
+        use.card->setFlags("zangfa");
+        foreach(ServerPlayer *q, room->getAlivePlayers()) {
+            if (!use.to.contains(q) && !use.from->isProhibited(q, use.card) && use.card->targetFilter(QList<const Player *>(), q, use.from)) {
+                invoke = true;
+                break;
+            }
+        }
+        use.card->setFlags("-zangfa");
+        use.card->setFlags("-IgnoreFailed");
+        if (!invoke)
+            return QList<SkillInvokeDetail>();
+
+        QList<SkillInvokeDetail> d;
+        QList<ServerPlayer *> invokers = room->findPlayersBySkillName(objectName());
+        foreach (ServerPlayer *p, invokers)
+        {
+            if (p->isAlive() && (p == use.from || use.to.contains(p)))
+                d << SkillInvokeDetail(this, p, p);
+
+        }
+        return d;
+    }
+
+    bool cost(TriggerEvent, Room *room, QSharedPointer<SkillInvokeDetail>invoke, QVariant &data) const
+    {
+
+        QList<ServerPlayer *> listt;
+        CardUseStruct use = data.value<CardUseStruct>();
+        use.card->setFlags("IgnoreFailed");
+        use.card->setFlags("zangfa");
+        foreach(ServerPlayer *q, room->getAlivePlayers()) {
+            if (!use.to.contains(q) && !use.from->isProhibited(q, use.card) && use.card->targetFilter(QList<const Player *>(), q, use.from))
+                listt << q;
+        }
+        use.card->setFlags("-zangfa");
+        use.card->setFlags("-IgnoreFailed");
+        ServerPlayer *target = room->askForPlayerChosen(invoke->invoker, listt, objectName(), "@zangfa", true, true);
+        //player->tag.remove("huanshi_source");
+        if (target) {
+            invoke->targets << target;
+            return true;
+        }
+        return false;
+    }
+
+    bool effect(TriggerEvent, Room *room, QSharedPointer<SkillInvokeDetail>invoke, QVariant &data) const
+    {
+        CardUseStruct use = data.value<CardUseStruct>();
+        use.to << invoke->targets.first();
+        room->sortByActionOrder(use.to);
+        data = QVariant::fromValue(use);
+        return false;
+    }
+};
+
+class ZangfaDistance : public TargetModSkill
+{
+public:
+    ZangfaDistance()
+        : TargetModSkill("zangfa-dist")
+    {
+        pattern = "TrickCard";
+    }
+
+    int getDistanceLimit(const Player *, const Card *card) const
+    {
+        if (card->hasFlag("zangfa"))
+            return 1000;
+
+        return 0;
+    }
+};
 
 TH16Package::TH16Package()
     : Package("th16")
@@ -170,7 +328,8 @@ TH16Package::TH16Package()
     Q_UNUSED(aun);
 
     General *narumi = new General(this, "narumi", "tkz");
-    Q_UNUSED(narumi);
+    narumi->addSkill(new Puti);
+    narumi->addSkill(new Zangfa);
 
     General *satono = new General(this, "satono", "tkz");
     Q_UNUSED(satono);
@@ -180,7 +339,7 @@ TH16Package::TH16Package()
 
 
     addMetaObject<MenfeiCard>();
-
+    skills << new ZangfaDistance;
 }
 
 ADD_PACKAGE(TH16)
