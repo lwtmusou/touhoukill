a3993c0a2674aad356dc380c890aca3275d79ef0
lwtmusou <lwtmusou@126.com>
2019-10-29 23:10:14 +0900

    lang

--- lang/zh_CN/Package/HegemonyCardPackage.lua
+++ lang/zh_CN/Package/HegemonyCardPackage.lua
@@ -8,10 +8,10 @@ return {
 	[":SixSwords"] = "装备牌·武器\n\n攻击范围：2<br/>技能：锁定技，与你阵营相同的其他角色的攻击范围+1。",
 	
 	["befriend_attacking"] = "远交近攻",
-	[":befriend_attacking"] = "锦囊牌\n\n使用时机：出牌阶段。\n使用目标：与你阵营不同的一名有明置的武将牌的角色。\n作用效果：你摸三张牌，然后目标角色摸一张牌。",
+	[":befriend_attacking"] = "锦囊牌\n\n使用时机：出牌阶段。\n使用目标：与你阵营不同的一名有明置的武将牌的角色。\n作用效果：你摸<font color=\"green\"><b>三张</b></font>牌，然后目标角色摸一张牌。",
 
 	["await_exhausted_hegemony"] = "以逸待劳(国)",
-	[":await_exhausted_hegemony"] = "锦囊牌\n\n使用时机：出牌阶段。\n使用目标：你和与你阵营相同的所有角色。\n作用效果：每名目标角色摸两张牌，然后每名目标角色弃置两张牌。",
+	[":await_exhausted_hegemony"] = "锦囊牌\n\n使用时机：出牌阶段。\n使用目标：你和与你阵营相同的所有角色。\n作用效果：每名目标角色摸<font color=\"green\"><b>两张</b></font>牌，然后每名目标角色弃置两张牌。",
 
 	["heg_nullification"] = "无懈可击·国",
 	[":heg_nullification"] = "锦囊牌\n\n使用方法Ⅰ：\n使用时机：一张锦囊牌对一个目标生效前。\n使用目标：一张对一个目标生效前的锦囊牌。\n作用效果：抵消此锦囊牌。"
@@ -28,7 +28,7 @@ return {
 	
 	
 	["known_both_hegemony"] = "知己知彼（国）",
-	[":known_both_hegemony"] = "锦囊牌·非全体性的普通锦囊牌<br />使用时机：出牌阶段。<br />使用目标：一名有手牌或暗置人物牌的其他角色。<br />作用效果：你选择：<br />1.观看目标角色的一张暗置人物牌及身份牌 <br /> 2.观看目标角色手牌。<br />重铸时机：出牌阶段。<br />◆重铸：声明一张牌并将之置入处理区，然后将之置入弃牌堆，最后摸一张牌。",
+	[":known_both_hegemony"] = "锦囊牌·非全体性的普通锦囊牌<br />使用时机：出牌阶段。<br />使用目标：一名有手牌或暗置人物牌的其他角色。<br />作用效果：你选择<font color=\"green\"><b>一项</b></font>：<br />1.观看目标角色的一张暗置人物牌 <br /> 2.观看目标角色手牌。<br />重铸时机：出牌阶段。<br />◆重铸：声明一张牌并将之置入处理区，然后将之置入弃牌堆，最后摸一张牌。",
 	["known_both_hegemony:showhead"] = "主将",
 	["known_both_hegemony:showdeputy"] = "副将",
 	["known_both_hegemony:showcard"] = "手牌",
