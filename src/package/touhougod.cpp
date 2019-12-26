3a6314528a33bcb4dc33b019afe607cad3f50510
lwtmusou <lwtmusou@126.com>
2019-12-26 22:38:23 +0900

    某天子的神天子
    实现倒是实现了，操作贼麻烦

--- src/package/touhougod.cpp
+++ src/package/touhougod.cpp
@@ -7119,153 +7119,191 @@ public:
 };
 
 
-/*
+
 class Tianbian : public TriggerSkill
 {
 public:
-Tianbian()
-: TriggerSkill("tianbian")
-{
-events << EventPhaseProceeding;
-frequency = Enteral;
-}
+    Tianbian()
+        : TriggerSkill("tianbian")
+    {
+        events << EventPhaseProceeding;
+        frequency = Eternal;
+    }
 
 
-QList<SkillInvokeDetail> triggerable(TriggerEvent triggerEvent, const Room *, const QVariant &data) const
+QList<SkillInvokeDetail> triggerable(TriggerEvent triggerEvent, const Room *room, const QVariant &data) const
 {
-ServerPlayer *current = data.value<ServerPlayer *>();
-if (current == NULL || current->isDead())
-return QList<SkillInvokeDetail>();
-if (current->getPhase() == Player::Judge) {
-QList<const Card *> tricks = current->getJudgingArea();
-if (tricks.isEmpty())
-return QList<SkillInvokeDetail>();
-QList<ServerPlayer *> tenshis = room->findPlayersBySkillName(objectName());
-QList<SkillInvokeDetail> d;
-foreach(ServerPlayer *tenshi, tenshis)
-d << SkillInvokeDetail(this, tenshi, tenshi, NULL, true);
-return d;
-}
-return QList<SkillInvokeDetail>();
+    ServerPlayer *current = data.value<ServerPlayer *>();
+    if (current == NULL || current->isDead())
+        return QList<SkillInvokeDetail>();
+    if (current->getPhase() == Player::Judge) {
+        QList<const Card *> tricks = current->getJudgingArea();
+        if (tricks.isEmpty())
+            return QList<SkillInvokeDetail>();
+        QList<ServerPlayer *> tenshis = room->findPlayersBySkillName(objectName());
+        QList<SkillInvokeDetail> d;
+        foreach(ServerPlayer *tenshi, tenshis)
+            d << SkillInvokeDetail(this, tenshi, tenshi, NULL, true);
+        return d;
+    }
+    return QList<SkillInvokeDetail>();
 }
 
 bool effect(TriggerEvent triggerEvent, Room *room, QSharedPointer<SkillInvokeDetail> invoke, QVariant &data) const
 {
-ServerPlayer *current = data.value<ServerPlayer *>();
-int maxchoice = qMax(current->getJudgingAreaID().length(), 2);
-//step 1 choose card which will not be used
-QList<int> disable;
-for (int i = 0; i < maxchoice; i += 1) {
-int card_id = room->askForCardChosen(invoke->invoker, current, "j", objectName(), visible, Card::MethodDiscard, disable); //refuasable??
-if (card_id <= -1)
-break;
-disable << card_id;
+    ServerPlayer *current = data.value<ServerPlayer *>();
+    int maxchoice = qMax(current->getJudgingAreaID().length(), 2);
+    //step 1 choose card which will not be used
+    QList<int> disable;
+    for (int i = 0; i < maxchoice; i += 1) {
+        if (!invoke->invoker->askForSkillInvoke("tianbian_choosecard"))
+            break;
+        
+        int card_id = room->askForCardChosen(invoke->invoker, current, "j", objectName(), false, Card::MethodDiscard, disable); //refuasable??
+        if (card_id <= -1)
+            break;
+        disable << card_id;
 
-}
-//step 2 use card which is avaliable
-QList<int> tricks = current->getJudgingAreaID();
-QList<int> effected;
-while (!tricks.isEmpty() && current->isAlive()) {
-tricks = current->getJudgingAreaID();
-//foreach(int id, effected) {
-//	if (tricks.contains(id))
-//		tricks.removeOne(id);
-//}
+    }
 
-QList<int> use_ids, disable_use_ids;
-foreach(int id, tricks) {
-const Card *c = Sanguosha->getEngineCard(id) //deletelater?
-c->setFlags("tianbian");
-c->setFlags("IgnoreFailed");
-bool can = !disable.contains(id) && usecheck (invoke->invoker, current, c);
-c->setFlags("-IgnoreFailed");
-c->setFlags("-tianbian");
-if (can)
-use_ids << id;
-else
-disable_use_ids << id;
+    //step 2 use card which is avaliable
+    QList<int> tricks = current->getJudgingAreaID();
+    QList<int> effected;
+    while (!tricks.isEmpty() && current->isAlive()) {
+        tricks = current->getJudgingAreaID();
+        //foreach(int id, effected) {
+        //	if (tricks.contains(id))
+        //		tricks.removeOne(id);
+        //}
 
-}
-if (use_ids.length() == 0)
-break;
-int card_id = room->askForCardChosen(invoke->invoker, current, "j", objectName(), visible, Card::MethodDiscard, disable_use_ids);
-const Card *c = Sanguosha->getEngineCard(card_id)
-//real delaycard??
-room->useCard(CardUseStruct(c, invoke->invoker, current));
-effected << card_id;
-}
-//step 3 throw all card, and effect delay
-if 	(current->isAlive() &&  !current->getJudgingAreaID().isEmpty()){
-int num = (current->getJudgingAreaID().length() / 2;
-DummyCard *dummy = new DummyCard;
-dummy->addSubcards(current->getJudgingAreaID());
-room->throwCard(dummy, current, invoke->invoker);
-QStringList names;
-names << "lightning"
-<< "indulgence"
-<< "supply_shortage"
-<< "saving_energy"
-<< "spring_breath";
+        QList<int> use_ids, disable_use_ids;
+        foreach(int id, tricks) {
+            const Card *c = Sanguosha->getEngineCard(id); //deletelater?
+            c->setFlags("tianbian");
+            c->setFlags("IgnoreFailed");
+            bool can = !disable.contains(id) && usecheck (invoke->invoker, current, c);
+            c->setFlags("-IgnoreFailed");
+            c->setFlags("-tianbian");
+            if (can)
+                use_ids << id;
+            else
+                disable_use_ids << id;
 
-//if (num > 0){
-for (int i = 0; i < num; i += 1) {
-QString choice = room->askForChoice(invoke->invoker, "qizhi", names.join("+"), QVariant());
-//if (exclude == "lightning") {
-Lightning *c = new Lightning(Card::NoSuit, 0);
-bool on_effect = room->cardEffect(c, NULL, player);  // iscanceled  ->  DelayedTrick::onEffect
-//effected << trick;
-//if (!on_effect)
-//	trick->onNullified(player); just for move delaytrick card
+        }
+        if (use_ids.length() == 0)
+            break;
+        int card_id = room->askForCardChosen(invoke->invoker, current, "j", objectName(), false, Card::MethodNone, disable_use_ids);
+        const Card *c = Sanguosha->getEngineCard(card_id);
+        //real delaycard??
+        room->useCard(CardUseStruct(c, invoke->invoker, current));
+        effected << card_id;
+    }
 
-JudgeStruct judge_struct = c->getJudge();
-judge_struct.who = current;
-room->judge(judge_struct);
+    //step 3 throw all card, and effect delay
+    if 	(current->isAlive() &&  !current->getJudgingAreaID().isEmpty()){
+        int num = current->getJudgingAreaID().length() / 2;
+        DummyCard *dummy = new DummyCard;
+        dummy->addSubcards(current->getJudgingAreaID());
+        room->throwCard(dummy, current, invoke->invoker);
+        QStringList names;
+        names << "lightning"
+        << "indulgence"
+        << "supply_shortage"
+        << "saving_energy"
+        << "spring_breath";
 
-if (judge_struct.negative == judge_struct.isBad()) {
-if (judge_struct.who->isAlive() && !judge_struct.ignore_judge)
-c->takeEffect(judge_struct.who);
-}
-delete c;
-//}
-}
-//}
-}
+        
+        for (int i = 0; i < num; i += 1) {
+            QString choice = room->askForChoice(invoke->invoker, objectName(), names.join("+"), QVariant());
+            tianbianEffect(choice, current);
+        }
+
+    }
 
 return false;
 }
+    static void tianbianEffect(QString choice, ServerPlayer *current) {
+        DelayedTrick *delay_trick;
+        Room *room = current->getRoom();
+        if (choice == "lightning") {
+            Lightning *c = new Lightning(Card::NoSuit, 0);
+            c->deleteLater();
+            delay_trick = qobject_cast<DelayedTrick *>(c);
+        }
+        else if (choice == "indulgence") {
+            Indulgence *c = new Indulgence(Card::NoSuit, 0);
+            c->deleteLater();
+            delay_trick = qobject_cast<DelayedTrick *>(c);
+        }
+        else if (choice == "supply_shortage") {
+            SupplyShortage *c = new SupplyShortage(Card::NoSuit, 0);
+            c->deleteLater();
+            delay_trick = qobject_cast<DelayedTrick *>(c);
+        }
+        else if (choice == "saving_energy") {
+            SavingEnergy *c = new SavingEnergy(Card::NoSuit, 0);
+            c->deleteLater();
+            delay_trick = qobject_cast<DelayedTrick *>(c);
+        }
+        else if (choice == "spring_breath") {
+            SpringBreath *c = new SpringBreath(Card::NoSuit, 0);
+            c->deleteLater();
+            delay_trick = qobject_cast<DelayedTrick *>(c);
+        }
+        if (delay_trick != NULL)
+        {
+            //bool on_effect = room->cardEffect(delay_trick, NULL, current);  // iscanceled  ->  DelayedTrick::onEffect
+            //effected << trick;
+            //if (!on_effect)
+            //	trick->onNullified(player); just for move delaytrick card
 
-static bool usecheck (ServerPlayer *from ,  ServerPlayer *to, const Card *c){
-if (c->isKindOf("Jink") || c->isKindOf("Nullification"))//using matchpattern is better?
-return false
-if (from->isCardLimited(c, Card::MethodUse) || from->isProhibited(to, c))
-return false
-if (c->isKindOf("Peach")) {
-if (to->isWounded())
-return true
-} else if (c->targetFilter(QList<const Player *>(), to, from))
-return true
-return false
-}
+            JudgeStruct judge_struct = delay_trick->getJudge();
+            judge_struct.who = current;
+            room->judge(judge_struct);
+
+            if (judge_struct.negative == judge_struct.isBad()) {
+                if (judge_struct.who->isAlive() && !judge_struct.ignore_judge)
+                    delay_trick->takeEffect(judge_struct.who);
+            }
+            delete delay_trick;
+        }
+        
+    }
+
+
+    static bool usecheck (ServerPlayer *from ,  ServerPlayer *to, const Card *c){
+        if (c->isKindOf("Jink") || c->isKindOf("Nullification"))//using matchpattern is better?
+            return false;
+        if (from->isCardLimited(c, Card::MethodUse) || from->isProhibited(to, c))
+            return false;
+        if (c->isKindOf("Peach")) {
+            if (to->isWounded())
+                return true;
+        }
+        else if (c->targetFilter(QList<const Player *>(), to, from))
+            return true;
+        return false;
+    }
 };
 
 class TianbianDistance : public TargetModSkill
 {
 public:
-TianbianDistance()
-: TargetModSkill("tianbian-dist")
-{
-pattern = "Slash,TrickCard+^DelayedTrick";
-}
+    TianbianDistance()
+        : TargetModSkill("#tianbian-dist")
+    {
+        pattern = "Slash,TrickCard+^DelayedTrick";
+    }
 
-int getDistanceLimit(const Player *, const Card *card) const
-{
-if (card->hasFlag("tianbian"))
-return 1000;
+    int getDistanceLimit(const Player *, const Card *card) const
+    {
+        if (card->hasFlag("tianbian"))
+            return 1000;
 
-return 0;
-}
+        return 0;
+    }
 };
-*/
+
 //skills  << new TianbianDistance
 
 
@@ -7433,6 +7471,9 @@ TouhouGodPackage::TouhouGodPackage()
 
     General *tenshi_god_sp = new General(this, "tenshi_god_sp", "touhougod", 4);
     tenshi_god_sp->addSkill(new QizhiAlernative);
+    tenshi_god_sp->addSkill(new Tianbian);
+    tenshi_god_sp->addSkill(new TianbianDistance);
+    related_skills.insertMulti("tianbian", "#tianbian-dist");
 
     //    General *shinmyoumaru_god = new General(this, "shinmyoumaru_god", "touhougod", 4, false, true, true);
     //    Q_UNUSED(shinmyoumaru_god);
