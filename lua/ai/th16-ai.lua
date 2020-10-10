dea170e26edb427a26592612077052bf64b7379e
lwtmusou@126.com <lwtmusou@126.com>
2020-10-10 10:56:58 +0900

    坂田合欢 简易ai

---
+++ lua/ai/th16-ai.lua
@@ -0,0 +1,30 @@
+
+--天空璋：坂田合欢
+--[磨刀]
+local modao_skill={}
+modao_skill.name="modao"
+table.insert(sgs.ai_skills,modao_skill)
+modao_skill.getTurnUseCard=function(self)
+	local cards = self.player:getCards("hs")
+	cards=self:touhouAppendExpandPileToList(self.player, cards)
+	cards=sgs.QList2Table(cards)
+	local card
+	self:sortByUseValue(cards,true)
+	for _,acard in ipairs(cards) do
+		if acard:getSuit() == sgs.Card_Club then
+			card = acard
+			break
+		end
+	end
+	if not card then return nil end
+	local suit = card:getSuitString()
+	local number = card:getNumberString()
+	local card_id = card:getEffectiveId()
+	local card_str = ("bone_healing:fengxiang[%s:%s]=%d"):format(suit, number, card_id)
+	local skillcard = sgs.Card_Parse(card_str)
+	assert(skillcard)
+	return skillcard
+end
+sgs.ai_cardneed.modao = function(to, card, self)
+	return card:getSuit() == sgs.Card_Club
+end
