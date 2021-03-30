4baf63be872cf5ff073b01e688d6a7aaa56081fa
Fs <Fsu0413@users.noreply.github.com>
2021-03-30 22:04:25 +0800

    版本号

--- src/core/engine.cpp
+++ src/core/engine.cpp
@@ -603,7 +603,7 @@ SkillCard *Engine::cloneSkillCard(const QString &name) const
 
 QString Engine::getVersionNumber() const
 {
-    return "20210221";
+    return "20210331";
 }
 
 QString Engine::getVersion() const
@@ -613,12 +613,12 @@ QString Engine::getVersion() const
 
 QString Engine::getVersionName() const
 {
-    return "V0.10.1";
+    return "V0.10.2";
 }
 
 QVersionNumber Engine::getQVersionNumber() const
 {
-    return QVersionNumber(0, 10, 1);
+    return QVersionNumber(0, 10, 2);
 }
 
 QString Engine::getMODName() const
