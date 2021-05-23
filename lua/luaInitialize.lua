29af69280cb84ad328ee307cdbda7f4b5dbf9297
xusine <xusine@outlook.com>
2021-05-23 17:08:59 +0200

    Loading tl with type checking

--- lua/luaInitialize.lua
+++ lua/luaInitialize.lua
@@ -129,7 +129,87 @@ dofile("qrc:/utilities.lua")
 -- 3. load extensions
 sgs_ex = require("sgs_ex")
 tl = require("tl")
-tl.loader()
+do 
+    local report_all_errors
+    do
+        local function report_errors(category, errors)
+            if not errors then
+                return false
+            end
+            if #errors > 0 then
+                local n = #errors
+                print("========================================")
+                print(n .. " " .. category .. (n ~= 1 and "s" or "") .. ":")
+                for _, err in ipairs(errors) do
+                    print(err.filename .. ":" .. err.y .. ":" .. err.x .. ": " .. (err.msg or ""))
+                end
+                return true
+            end
+            return false
+        end
+    
+       report_all_errors = function(env, syntax_only)
+          local any_syntax_err, any_type_err, any_warning
+          for _, name in ipairs(env.loaded_order) do
+             local result = env.loaded[name]
+    
+             local syntax_err = report_errors("syntax error", result.syntax_errors)
+             if syntax_err then
+                any_syntax_err = true
+             elseif not syntax_only then
+                any_warning = report_errors("warning", result.warnings) or any_warning
+                any_type_err = report_errors("error", result.type_errors) or any_type_err
+             end
+          end
+          local ok = not (any_syntax_err or any_type_err)
+          return ok, any_syntax_err, any_type_err, any_warning
+       end
+    end
+    
+    local function type_check_and_load(module_name)
+        local found_filename, fd, tried = tl.search_module(module_name, false)
+
+        if not found_filename then 
+            table.concat(tried, "\n\t")
+        end
+
+        fd:close()
+
+        if not tl.package_loader_env then
+            tl.package_loader_env = tl.init_env(found_filename:match("lua$"))
+        end
+
+        local result, err = tl.process(found_filename, tl.package_loader_env)
+        if err then
+            die(err)
+        end
+    
+       local is_tl = found_filename:match("%.tl$")
+       local _, syntax_err, type_err = report_all_errors(tl.package_loader_env, not is_tl)
+    
+       local chunk; 
+       chunk, err = (loadstring or load)(tl.pretty_print_ast(result.ast), "@" .. found_filename)
+       if chunk then
+         return function()
+            local ret = chunk()
+            package.loaded[module_name] = ret
+            return ret
+         end
+      else
+         error("Internal Compiler Error: Teal generator produced invalid Lua. Please report a bug at https://github.com/teal-language/tl\n\n" .. err)
+      end
+    end
+
+    -- append the loader into the environment.
+    if package.searchers then
+        table.insert(package.searchers, 2, type_check_and_load)
+    else
+        table.insert(package.loaders, 2, type_check_and_load)
+    end
+end
+
+
+-- Check the type. 
 
 dofile("lua/testtest.lua")
 
@@ -146,7 +226,7 @@ local loadExtension = function(name, isBuiltin)
     -- checksum check is done before this function is called
     -- each extension is a Lua table which has a key named 'type' and a value of 'sgs_ex.TableType.Package'
     -- should we require or dofile the table?
-    local extensionPrefix = "extension."
+    local extensionPrefix = "extensions."
     if isBuiltin then
         extensionPrefix = "builtinExtension."
     end
