41efa7e81f7569d47d51ac980a56eee50e100990
xusine <xusine@outlook.com>
2021-11-07 13:18:14 +0100

    askForExchange: Forget to add subcards.

--- src/server/room.cpp
+++ src/server/room.cpp
@@ -5774,6 +5774,7 @@ const Card *Room::askForExchange(ServerPlayer *player, const QString &reason, in
         return nullptr;
 
     Card *card = cloneCard();
+    card->addSubcards(IDSet(to_exchange.begin(), to_exchange.end()));
     ChoiceMadeStruct s;
     s.player = player;
     s.type = ChoiceMadeStruct::CardExchange;
