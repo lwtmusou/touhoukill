0edd995f27faf4f5a57736a6765c80960b8e856b
Fs <Fsu0413@vip.qq.com>
2022-10-15 16:53:54 +0800

    remove unimplemented functions (from merge)

--- src/server/server.h
+++ src/server/server.h
@@ -33,10 +33,6 @@ private:
     static const QMap<QSanProtocol::CommandType, SocketVerificationCallback> VerificationCallbacks;
     static const QMap<QSanProtocol::CommandType, SocketCallback> Callbacks;
 
-private:
-    void getLack(ClientSocket *socket);
-    void getWinnersTableFile(ClientSocket *socket, const QString &tableName);
-
 private slots:
     void socketConnected(QSgsMultiSocket *socket);
     void socketReadyRead();
