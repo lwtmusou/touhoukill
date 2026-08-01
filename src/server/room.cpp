dda33f16e6c626b99f2a4c5e39020d5e8466d053
Fs <Fsu0413@vip.qq.com>
2026-08-01 17:33:18 +0800

    fix loseHp with lost < 0
    Signed-off-by: Fs <Fsu0413@vip.qq.com>

--- src/server/room.cpp
+++ src/server/room.cpp
@@ -6766,10 +6766,12 @@ void Room::makeDamage(const QString &source, const QString &target, QSanProtocol
         return;
     // damage
     if (nature == S_CHEAT_HP_LOSE) {
-        loseHp(targetPlayer, point);
+        if (point > 0)
+            loseHp(targetPlayer, point);
         return;
     } else if (nature == S_CHEAT_MAX_HP_LOSE) {
-        loseMaxHp(targetPlayer, point);
+        if (point > 0)
+            loseMaxHp(targetPlayer, point);
         return;
     } else if (nature == S_CHEAT_HP_RECOVER) {
         RecoverStruct r;
